e102654998591a627fcdb8217db3f85854702596
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-08-24 15:13:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do 
+    erb :user_input
+  end
+  
+  post '/latinized' do
+    @latinize = PigLatinizer.new(params[:user_text])
+    
+    erb :display
+  end
 end
